a499dd971f2d9b592d80c0291bc6752e89777f17
Petr Los <petrlos@gmail.com>
2021-12-02 06:23:55 +0100

    Day 2 working

--- 02/02.py
+++ 02/02.py
@@ -1,8 +1,52 @@
 #Advent of Code 2021: Day 2
+class Position:
+    def __init__(self):
+        self.vertical = 0
+        self.horizontal = 0
+        self.aim = 0
+    def __str__(self):
+        return "{0}, {1}".format(self.horizontal, self.vertical)
+    def result(self):
+        return self.vertical * self.horizontal
 
-with open("test.txt") as file:
+def task1(instruction, step, position):
+    if instruction == "forward":
+        position.horizontal+= int(step)
+    elif instruction == "down":
+        position.vertical += int(step)
+    elif instruction == "up":
+        position.vertical -= int(step)
+    return position
+
+def task2(instruction, step, position):
+    if instruction == "forward":
+        position.horizontal+= int(step)
+        position.vertical += int(step) * position.aim
+    elif instruction == "down":
+        position.aim += int(step)
+    elif instruction == "up":
+        position.aim -= int(step)
+    return position
+
+with open("data.txt") as file:
     lines = file.read().splitlines()
 
 print("AoC 2021 Day 2: ")
 
-print("pokus")
+instructions = [x.split(" ") for x in lines]
+
+#task1
+position = Position()
+for instruction in instructions:
+    direction, distance = instruction
+    position = task1(direction, distance, position)
+task1 = position.result()
+print("Task 1: ",task1)
+
+#task2
+position = Position()
+for instruction in instructions:
+    direction, distance = instruction
+    position = task2(direction, distance, position)
+task1 = position.result()
+print("Task 2: ", task1)
