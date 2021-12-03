fb41bddd3191dc356686097e8a99eb8b17d2eeb8
Petr Los <petrlos@gmail.com>
2021-12-03 21:40:25 +0100

    Day 3 Task 1+2 working

--- 03/03.py
+++ 03/03.py
@@ -1,9 +1,21 @@
 #Advent of Code 2021: Day 3
 from collections import Counter
 
-def findMostCommonNumber(column):
+def findMostCommonNumber(column, preference):
     occurenceCount = Counter(column)
-    return occurenceCount.most_common(1)[0][0]
+    if occurenceCount["1"] > occurenceCount["0"]:
+        return "1"
+    elif occurenceCount["1"] == occurenceCount["0"]:
+        return "1"
+    elif occurenceCount["1"] < occurenceCount["0"]:
+        return "0"
+
+def numbersRemaining(numbers):
+    counter = 0
+    for status in numbers.keys():
+        if numbers[status]:
+            counter += 1
+    return counter
 
 with open("data.txt") as file:
     lines = file.read().splitlines()
@@ -14,7 +26,7 @@ columns = zip(*lines[::-1])
 gamaRate = ""
 epsilonRate = ""
 for column in columns:
-    newNumber = findMostCommonNumber(column)
+    newNumber = findMostCommonNumber(column, "")
     gamaRate += newNumber
     if newNumber == "0":
         epsilonRate += "1"
@@ -22,3 +34,48 @@ for column in columns:
         epsilonRate += "0"
 
 print("Task 1:", int(gamaRate, 2) * int(epsilonRate, 2))
+
+numbers = {}
+for number in lines:
+    numbers.setdefault(number, True)
+
+position = 0
+while numbersRemaining(numbers) > 1:
+    subString = ""
+    for number in numbers.keys():
+        if numbers[number]:
+            subString += number[position]
+    mostCommon = findMostCommonNumber(subString, "1")
+    for number in numbers.keys():
+        if numbers[number]:
+            if number[position] != mostCommon:
+                numbers[number] = False
+    #printRemaining(numbers)
+    position += 1
+
+for number in numbers.keys():
+    if numbers[number]:
+        oxGenRating = int(number,2)
+
+numbers = {}
+for number in lines:
+    numbers.setdefault(number, True)
+
+position = 0
+while numbersRemaining(numbers) > 1:
+    subString = ""
+    for number in numbers.keys():
+        if numbers[number]:
+            subString += number[position]
+    mostCommon = findMostCommonNumber(subString, "0")
+    for number in numbers.keys():
+        if numbers[number]:
+            if number[position] == mostCommon:
+                numbers[number] = False
+    position += 1
+
+for number in numbers.keys():
+    if numbers[number]:
+        CO2ScrubRating = int(number,2)
+
+print("Task 2:", oxGenRating*CO2ScrubRating)
