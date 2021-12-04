3a9826456e9ec93c50ca9b57f9ea0f5b87cd8581
Petr Los <petrlos@gmail.com>
2021-12-04 12:38:27 +0100

    Day 4 completed

--- 04/04.py
+++ 04/04.py
@@ -12,32 +12,40 @@ def markNumber(board, drawnNumber):
                 board[rowIndex][columnIndex] = None
 
 def boardCompleted(board):
-    #checkRows
     for row in board:
         if len(set(row)) == 1:
             return True
     boardRotated = list(zip(*board[::-1]))
     for column in boardRotated:
         if len(set(column)) == 1:
-            print("got you", column)
             return True
     return False
+
+def createBoard(lines):
+    # third row and more
+    boardIndex, index = 0, 0
+    newBoard, boards = [], []
+    for line in lines[1:]:
+        newBoard.append([int(x) for x in regNum.findall(line)])
+        index += 1
+        if index == 6:
+            boards.append(newBoard[1:])  # ignore first row - empty
+            newBoard = []
+            index = 0
+    return boards
+
+def remainingNumbersSum(board):
+    result = 0
+    for row in board:
+        result += sum(filter(None, row))
+    return result
 
 #MAIN
 #first row only
 numbers = [int(x) for x in lines[0].split(",")]
 
-#third row and more
-boardIndex, index = 0, 0
-newBoard, boards = [], []
-for line in lines[1:]:
-    newBoard.append([int(x) for x in regNum.findall(line)])
-    index +=1
-    if index == 6:
-        boards.append(newBoard[1:]) #ignore first row - empty
-        newBoard = []
-        index = 0
-
+"""#Task1
+boards = createBoard(lines)
 weHaveAWinner = False
 numberIndex = 0
 while not weHaveAWinner:
@@ -49,11 +57,24 @@ while not weHaveAWinner:
             weHaveAWinner = True
             winningBoardIndex = boardIndex
     numberIndex += 1
-
 #get sum of remaining numbers:
-result = 0
-for row in boards[winningBoardIndex]:
-    result += sum(filter(None, row))
+sumOfRemainingNumbers = remainingNumbersSum(boards[winningBoardIndex])
+print("Task 1:",sumOfRemainingNumbers*drawnNumber)"""
 
-#Task1 - product last drawn number and sum of remaining numbers
-print("Task 1:",result*drawnNumber)
+#Task1 + 2
+boards = createBoard(lines)
+results = []
+for number in numbers:
+    boardComplete = False
+    for boardIndex, board in enumerate(boards):
+        if board != None:
+            markNumber(board, number)
+            boardComplete = boardCompleted(board) #check if board completed
+        if boardComplete:
+            numbersSum = remainingNumbersSum(boards[boardIndex])
+            results.append(numbersSum*number)
+            boards[boardIndex] = None
+    boards = list(filter(lambda x: x != None, boards))
+
+print("Task 1:", results[0])
+print("Task 2:", results[-1])
