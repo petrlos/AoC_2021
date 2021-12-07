c461af96195254eb0b130da2c0a5a0a417edc8b4
Petr Los <petrlos@gmail.com>
2021-12-07 06:46:59 +0100

    Day 7 both stars working

--- 07/07.py
+++ 07/07.py
@@ -3,16 +3,21 @@ from datetime import datetime
 start = datetime.now()
 
 with open("data.txt") as file:
-    numbers = [int(x) for x in file.read().split(",")]
+    crabShips = [int(x) for x in file.read().split(",")]
+
+fuelCosts = []
+for possibleDepth in range(max(crabShips) + 1):
+    fuelCosts.append(sum(range(possibleDepth+1)))
 
 resultsTask1 = []
 resultsTask2 = []
-for depth in range(1, max(numbers)):
+for depth in range(0, max(crabShips)):
     checkSumTask1 = 0
     checkSumTask2 = 0
-    for number in numbers:
-        checkSumTask1 += abs(depth - number)
-        checkSumTask2 += sum(range(abs(number - depth)+1))
+    for crabShip in crabShips:
+        checkSumTask1 += abs(depth - crabShip)
+        # FIRST SOLUTION: 24 sek runtime: checkSumTask2 += sum(range(abs(number - depth)+1))
+        checkSumTask2 += fuelCosts[abs(depth - crabShip)]
     resultsTask1.append(checkSumTask1)
     resultsTask2.append(checkSumTask2)
 
