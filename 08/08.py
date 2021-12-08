c009d37b3ceb34570c0ee0053bb5cc5c4f8a0f63
Petr Los <petrlos@gmail.com>
2021-12-08 08:14:32 +0100

    Day 8 - Task1 running

--- 08/08.py
+++ 08/08.py
@@ -1,7 +1,21 @@
 # Advent of Code 2021: Day 8
 
-with open("test.txt") as file:
+with open("data.txt") as file:
     lines = file.read().splitlines()
 
-print(lines)
+signals = []
+outputs = []
+for line in lines:
+    signal, output = line.split(" | ")
+    signals.append(signal)
+    outputs.append(output)
 
+#Task1
+counter = 0
+oneFourSevenEight = [2, 4, 3, 7] #segments count needed to light up this number
+for output in outputs:
+    numbers = output.split(" ")
+    for number in numbers:
+        if len(number) in oneFourSevenEight:
+            counter += 1
+print("Task 1: ",counter)
