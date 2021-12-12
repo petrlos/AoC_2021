43f10d807da5e0e6dc8a9e18f6673f77272e9f82
Petr Los <petrlos@gmail.com>
2021-12-13 00:30:44 +0100

    Day 12: Star 2 working loooong

--- 12/12.py
+++ 12/12.py
@@ -1,5 +1,6 @@
 #Advent of Code 2021: Day 12
 from collections import deque
+from collections import Counter
 
 def findNextCave(location):
     destinations = deque()
@@ -26,6 +27,16 @@ def smallCaveVisitedMaxOnce(path):
                 return False
     return True
 
+def smallCaveTask2Checker(path):
+    smallCaves = list(filter(lambda x: x.islower(), path[1:-1])) #only small caves, not start+end
+    smallCavesCounter = list(Counter(smallCaves).values())
+    if len(smallCavesCounter):
+        if max(smallCavesCounter) > 2:
+            return False
+        if smallCavesCounter.count(2) > 1:
+            return False
+    return True
+
 def task1(start):
     queue = [[start]]
     completedLegalPaths = []
@@ -48,8 +59,32 @@ def task1(start):
         queue = list(filter(lambda x: x != "delete", queue))
     return len(completedLegalPaths)
 
+def task2(start):
+    queue = deque([[start]])
+    completedLegalPaths = 0
+    while queue:
+        for _ in range(len(queue)):
+            path = queue[0]
+            newCaves = findNextCave(path[-1])
+            newPaths = []
+            for newCave in newCaves:
+                newPath = path + [newCave]
+                newPaths.append(newPath)
+            queue += newPaths
+            queue.popleft()
+        for index, path in enumerate(queue):
+            if not smallCaveTask2Checker(path):
+                queue[index] = "delete"
+            if pathCompleted(queue[index]):
+                completedLegalPaths += 1
+                queue[index] = "delete"
+        queue = deque(list(filter(lambda x: x != "delete", queue)))
+        print(len(queue))
+    return completedLegalPaths
+
 #MAIN
-with open("data.txt") as file:
+with open("test3.txt") as file:
     lines = [x.split("-") for x in file.read().splitlines()]
 
 print("Task 1:",task1("start"))
+print("Task 2:",task2("start"))
