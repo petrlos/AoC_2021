30fba3756480abddad9592aff989b1d459bd0ae1
Petr Los <petrlos@gmail.com>
2022-01-02 21:38:06 +0100

    Day 19: Rotation working

--- 19/19.py
+++ 19/19.py
@@ -1,5 +1,5 @@
 #Advent of Code 2021: Day 19
-from copy import deepcopy
+from pprint import pprint
 
 def parseData(lines):
     scanner, scanners = [], []
@@ -14,15 +14,28 @@ def parseData(lines):
     return scanners
 
 def rotateByY(input, index):
+    #rotates only axe Y - 4 possible directions
     x, y, z = input
     rotateByYDict = {0:[x,y,z], 1:[-x,y,-z], 2:[-z, y, x], 3:[z, y, -x]}
     return rotateByYDict[index]
 
-def rotateToOtherSide(input, index):
+def rotateToOtherDirection(input, index):
+    #rotates the whole cube - 6 possible directions
     x, y, z = input
     rotateToOtherSideDict = {0:[x, y, z], 1:[y, z, x], 2:[ z, -y, x],
                              3:[x, -z,y], 4:[z, x, y], 5:[-x,-z, y]}
     return rotateToOtherSideDict[index]
+
+def rotateCompleteScanner(beacons, rotation):
+    #finds new coords for all beacons from scanner in ONE of 24 rotations
+    rotatedScanner = []
+    direction, YAxe = (rotation //4), (rotation % 4)
+    print(direction, YAxe)
+    for beacon in beacons:
+        beaconDirectionRotated = rotateToOtherDirection(beacon, direction)
+        beaconYrotated = rotateByY(beaconDirectionRotated, YAxe)
+        rotatedScanner.append(beaconYrotated)
+    return rotatedScanner
 
 #MAIN:
 with open("test2.txt") as file:
@@ -32,11 +45,5 @@ scanners = parseData(lines)
 
 
 for scanner in scanners:
-    for beacon in scanner:
-        for sideRotate in range(6):
-            scannerRotated= rotateToOtherSide(beacon, sideRotate)
-            print(sideRotate, end=" ")
-            for rotateY in range(4):
-                scannerRotatedY = rotateByY(scannerRotated, rotateY)
-                print(scannerRotatedY, end=" ")
-            print(" ")
+    for rotation in range(24):
+        rotatedScanner = rotateCompleteScanner(scanner, rotation)
