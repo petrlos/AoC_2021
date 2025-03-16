26cc1a3241f0bd896fd67c2e088408cdd3047e24
petrlos <petrlos@gmail.com>
2025-03-16 23:30:38 +0100

    Day 18: working logic done, explosion+split remains

--- 18/18.py
+++ 18/18.py
@@ -18,9 +18,16 @@ def add_snailfish_numbers(num1, num2):
     return [num1, num2]
 
 def explode(number):
+    if number == [[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]:
+        return [[[[0,7],4],[7,[[8,4],9]]],[1,1]]
+    if number == [[[[0,7],4],[7,[[8,4],9]]],[1,1]]:
+        return [[[[0,7],4],[15,[0,13]]],[1,1]]
+
     return -1
 
 def split(number):
+    if number == [[[[0,7],4],[15,[0,13]]],[1,1]]:
+        return [[[[0,7],4],[[7,8],[0,13]]],[1,1]]
     return -1
 
 #MAIN
@@ -32,29 +39,16 @@ numbers = [json.loads(json_string) for json_string in lines]
 result = numbers[0]
 for number in numbers[1:]:
     result = add_snailfish_numbers(result, number) #add new number to existing one
-    new_number = result
     reduced = False
     while not reduced:
-        explosive = True
-        changed = False
-        while explosive: #explsive loop - repeat until explosion possible
-            new_number = explode(result)
-            if new_number == -1: #not explosive anymore
-                explosive = False
-            else: #number exploded
-                result = new_number
-                changed = True
-
-        splittable = True
-        while splittable: #split loop
-            new_number = split(result)
-            if new_number == -1: #not splittable anymore
-                splittable = False
-            else: #number splitted
-                result = new_number
-                changed = True
-
-        if not changed: #nothing happened in booths inner loops - leave outer loop
-            reduced = True
+        exploded = explode(result) #try explode
+        if exploded != -1:  #if exploded
+            result = exploded
+        else: #not exploded - try split
+            splitted = split(result)
+            if splitted != -1: #if splitted
+                result = splitted
+            else:
+                reduced = True #not exploded and not splitted - number reduced
 
 print(result)
