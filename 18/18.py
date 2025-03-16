4821686219c5111adf4539a09f6f128b2ee2b7b4
petrlos <petrlos@gmail.com>
2025-03-16 22:57:27 +0100

    Day 18: working logic done, explosion+split remains

--- 18/18.py
+++ 18/18.py
@@ -14,6 +14,14 @@ def count_magnitude(number):
         result += 2* right
     return result
 
+def add_snailfish_numbers(num1, num2):
+    return [num1, num2]
+
+def explode(number):
+    return -1
+
+def split(number):
+    return -1
 
 #MAIN
 with open("test.txt") as file:
@@ -21,5 +29,32 @@ with open("test.txt") as file:
 
 numbers = [json.loads(json_string) for json_string in lines]
 
-for number in numbers:
-    print(count_magnitude(number))
+result = numbers[0]
+for number in numbers[1:]:
+    result = add_snailfish_numbers(result, number) #add new number to existing one
+    new_number = result
+    reduced = False
+    while not reduced:
+        explosive = True
+        changed = False
+        while explosive: #explsive loop - repeat until explosion possible
+            new_number = explode(result)
+            if new_number == -1: #not explosive anymore
+                explosive = False
+            else: #number exploded
+                result = new_number
+                changed = True
+
+        splittable = True
+        while splittable: #split loop
+            new_number = split(result)
+            if new_number == -1: #not splittable anymore
+                splittable = False
+            else: #number splitted
+                result = new_number
+                changed = True
+
+        if not changed: #nothing happened in booths inner loops - leave outer loop
+            reduced = True
+
+print(result)
